36c179eeba4ea4a6180ac5aba24fcf0fad246ecd
Robert Chen <robert.chen@thorntechnologies.com>
2016-05-16 20:53:39 -0400

    Initial Commit

---
+++ horizontalScroller-tvOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  horizontalScroller-tvOS
+//
+//  Created by Robert Chen on 5/16/16.
+//  Copyright © 2016 Robert Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
